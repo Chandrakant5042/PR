8a24f59301d8d171c7f4e24e0bda7651d3b54189
Chandrakant <chandrakantmkamble1998@gmail.com>
2023-02-02 11:39:33 +0530

    b1 added

---
+++ src/Pr3.java
@@ -0,0 +1,16 @@
+package PR1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Pr3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chromedriver.driver", "E:\\EclipseWorkspace\\lib\\chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.w3schools.com/");
+		sop("hi");
+	}
+
+}
+git 
